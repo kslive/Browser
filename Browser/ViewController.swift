d091d4b1ada457f7a3809f09f13eb602ceb2c840
Eugene Kiseleff <zhenya-kiselev-97@mail.ru>
2020-07-12 18:11:41 +0300

    Button Back/Forward + Text Field for URL.

--- Browser/ViewController.swift
+++ Browser/ViewController.swift
@@ -12,6 +12,9 @@ import WebKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var urlTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +24,14 @@ class ViewController: UIViewController {
         webView.load(request)
         webView.allowsBackForwardNavigationGestures = true
     }
-
-
+    
+    @IBAction func backButtonAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func forwardButtonAction(_ sender: Any) {
+        
+    }
+    
 }
 
